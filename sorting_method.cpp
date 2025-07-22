444632e7ab4939cae03ae9a0aad671dc3670e5f3
Mayank Kumar Gupta <165919021+MKG0007@users.noreply.github.com>
2025-07-22 23:17:21 +0530

    Update sorting_method.cpp

--- sorting_method.cpp
+++ sorting_method.cpp
@@ -18,6 +18,9 @@ three basic methods to sort the arrary:-
           
         }
     }
+
+note:-
+  time_complexity for all cases is O(n^2)
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------->
 
 bubble sort:- in this method we place the largest element on the right side of the array in every iteration.
@@ -39,6 +42,11 @@ bubble sort:- in this method we place the largest element on the right side of t
             }
         }
     }
+
+  note:
+  time_complexity in avg , wrost case is O(n^2)
+    but in best case O(n)
+    
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------->
 
 insertion sort:- in this method we take the element and place it into it correct place (by checking the left side of the array every iteration)
@@ -59,3 +67,99 @@ insertion sort:- in this method we take the element and place it into it correct
             
         }
     }
+note:-
+  time_complexity avg , wrost case is O(n^2)
+    but in best case O(n)
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------->
+
+  merge Sort:- it is the advanced sorting algorithem in which divide the array and sort the array and then agian combine it (this continues untill all parts become one sized)
+  steps for merge sort:
+    it considered of tow steps 1->(divide the array) , 2->(combine it in sorted manner)
+
+    void mergeParts(vector<int>&arr , int low , int mid , int high){//step-2(combine the array parts in the sorted manner) 
+        vector<int> temp;
+        int i = low;
+        int j = mid+1;
+        
+        while(i<=mid && j<=high){
+            if(arr[i]<=arr[j]){
+                temp.push_back(arr[i]);
+                i++;
+            }
+            else{
+                temp.push_back(arr[j]);
+                j++;
+            }
+        }
+        
+        while(i<=mid) {
+            temp.push_back(arr[i]);
+            i++;
+        }
+        
+        while(j<=high){
+            temp.push_back(arr[j]);
+            j++;
+        }
+        
+        for(int i = low ; i<=high ; i++){
+            arr[i] = temp[i-low];
+        }
+        
+    }
+    void mergeSort(vector<int>& arr, int l, int r) {//step - 1(dividing the array)
+        if(l>=r) return ;
+        
+        int mid = (l+r)/2;
+        
+        mergeSort(arr , l , mid);//recursively dividing the array's left part
+        mergeSort(arr , mid+1 , r);//recursively dividing the array's right part
+        mergeParts(arr , l  , mid , r);//at last combine both parts left and right
+    }
+
+time_complexity is O(nlog(n))
+space_complexity is O(n)
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------->
+
+
+  quick Sort:- it is the another advanced sorting algorithem in which we take any randome element as a pivot and place smaller into it left side or greater element into it right side.
+ it considered of three steps-> 
+  1)first select the pivot index  
+  2)arrange the element around it 
+  3)place the pivot into its right place 
+  4)call for its left and right parts 
+
+  void quickSort(vector<int>& arr, int low, int high) {
+        if(low<high){
+            int part_index = partition(arr , low , high);
+            
+            quickSort(arr , low , part_index-1);
+            quickSort(arr , part_index+1 , high);
+        }
+        
+    }
+
+    int partition(vector<int>& arr, int low, int high) {
+        int pivot = low;
+        int i = low ;
+        int j = high;
+        while(i<j){
+            while(arr[i]<=arr[pivot] && i<high) i++;
+            while(arr[j]>arr[pivot] && j>low) j--;
+            
+            if(i<j) swap(arr[i] , arr[j]);
+        }
+        swap(arr[pivot] , arr[j]);
+        return j;
+    }
+
+  time_complexity is O(nlog(n)) 
+  but in wrost case when pivot is selected poorly O(n^2)
+  space_complexity is O(1)
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------>
+    
+
+    
