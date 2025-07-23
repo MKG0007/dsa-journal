c8462aba82c005f05b92d5a29fdde8f32f27daec
Mayank Kumar Gupta <165919021+MKG0007@users.noreply.github.com>
2025-07-24 00:58:43 +0530

    Update dutch_national_flag_algo.cpp

--- array_Questions/dutch_national_flag_algo.cpp
+++ array_Questions/dutch_national_flag_algo.cpp
@@ -1,1 +1,32 @@
-hello i am mayank kumar gupta
+Dutch National Flag Algorithem :-
+  it is the well none method use to sort the array contain three types of nunbers.
+  step1-
+  define the pointers :- low , mid , high
+  from 0 to low-1 --> n1;
+  from low to mid-1 --> n2;
+  from mid to high --> contains un-sorted part of the array
+  from high+1 to n-1 --> n3;
+
+  code:-
+    void sortColors(vector<int>& nums) {
+        int size = nums.size();
+        //intially low = mid because from start whole array unsorted
+        int low = 0;
+        int mid = 0;
+        int high = size - 1;
+
+        while (mid <= high) {
+            if (nums[mid] == 0) {//swaping with low (assuming that the low pointing to '1'
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+        }
+    }
+
+time_complexity-->O(n)
