5ba832fe64b2761cd57270496ba687ed1de810d1
Mayank Kumar Gupta <165919021+MKG0007@users.noreply.github.com>
2025-10-06 22:48:12 +0530

    Update pathOfTheGivenNode.cpp

--- tree-questions/pathOfTheGivenNode.cpp
+++ tree-questions/pathOfTheGivenNode.cpp
@@ -1,1 +1,25 @@
 
+//find the paths of the given node from the root
+
+// by using inorder travesal
+bool paths(TreeNode* root vector<int> &temp , int leaf){
+        // finds instead of given node
+        //return false;
+        if(root == nullptr) return false;
+
+        temp.push_back(root->val);
+        // if we findout the given node there is no need to go further
+        if(root->val == leaf) return true;
+
+        // in any case the 
+        // if the given node is findout then return with true
+        if(paths(root->left , temp , leaf) || paths(root->right , temp , leaf)){
+            return true;
+        }
+
+        // the path is invalid so to removing the element at the time of back tracking
+        temp.pop_back();
+        return false;
+
+
+    }
